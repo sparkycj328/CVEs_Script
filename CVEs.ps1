ee91f42c05b5ddf37c5476e86dbd270e52775fbd
Carlos <sparkycj328@gmail.com>
2026-03-11 14:42:47 +0100

    Add logic for max CVSS score and EPSS score retrieval

--- CVEs.ps1
+++ CVEs.ps1
@@ -8,7 +8,8 @@ $FileContents = Get-Content -Path $FilePath
  
 # declare variables to be used during loop
 $i = 0
-$data = @{}
+$MaxData = [ordered]@{}
+$CpeToCveLookup = @{}
 $arr = @()
 
 # Read the file line by line FOR Cpe Name strings
@@ -18,47 +19,100 @@ ForEach ($Line in $FileContents) {
     $URL = "https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName=$Line&isVulnerable"
 
     # Use the stored CPE strings to retrieve any vulnerabilities
-    $AllRetrievedCVEs = Invoke-RestMethod -Uri $URL
-    
-    # grab the desired information from the vulenrability object
-    foreach ($item in $AllRetrievedCVEs.vulnerabilities) {
-        $cve = $item.cve
-        
-        if ($cve.metrics.cvssMetricV31)
-        {
-            if ($cve.metrics.cvssMetricV31[0].cvssData.baseScore -gt $max) {
-                $max = $cve.metrics.cvssMetricV31[0].cvssData.baseScore
-                $arr = @($cve.id, $cve.metrics.cvssMetricV31[0].cvssData.baseScore, $cve.metrics.cvssMetricV31[0].cvssData.vectorString)
-                $data[$Line] = $arr
-            }
-            
-        }
-        elseif ($cve.metrics.cvssMetricV30)
-        {
-
-            if ($cve.metrics.cvssMetricV30[0].cvssData.baseScore -gt $max) {
-                $max = $cve.metrics.cvssMetricV30[0].cvssData.baseScore
-                $arr = @($cve.id, $cve.metrics.cvssMetricV30[0].cvssData.baseScore, $cve.metrics.cvssMetricV30[0].cvssData.vectorString)
-                $data[$Line] = $arr
-            }
-        }
-        else {
-            if ($cve.metrics.cvssMetricV2[0].cvssData.baseScore -gt $max) {
-                $max = $cve.metrics.cvssMetricV2[0].cvssData.baseScore
-                $arr = @($cve.id, $cve.metrics.cvssMetricV2[0].cvssData.baseScore, $cve.metrics.cvssMetricV2[0].cvssData.vectorString)
-                $data[$Line] = $arr
-            }
-        }
+    try {
+        $AllRetrievedCVEs = Invoke-RestMethod -Uri $URL
+    } catch {
+        Write-Host "Status Code:" $_.Exception.Response.StatusCode.value__
+        Write-Host "CPE Name: " $Line 
     }
     
-    # fetch the EPSS score based on the CVE-ID
+    # grab the desired information from the vulenrability object
+    if (@(AllRetrievedCVEs.vulnerabilities).Count -gt 0) {
+        foreach ($item in $AllRetrievedCVEs.vulnerabilities) {
+            $cve = $item.cve
+            if ($cve.metrics.cvssMetricV31)
+            {
+                if ($cve.metrics.cvssMetricV31[0].cvssData.baseScore -gt $max) {
+                    $max = $cve.metrics.cvssMetricV31[0].cvssData.baseScore
+                    $arr = @($cve.id, $cve.metrics.cvssMetricV31[0].cvssData.baseScore, $cve.metrics.cvssMetricV31[0].cvssData.vectorString)
+                    $MaxData[$Line] = $arr
+                    $CpeToCveLookup[$cve.id] = $Line
+                }
+                
+            }
+            elseif ($cve.metrics.cvssMetricV31)
+            {
+                if ($cve.metrics.cvssMetricV31[0].cvssData.baseScore -gt $max) {
+                    $max = $cve.metrics.cvssMetricV31[0].cvssData.baseScore
+                    $arr = @($cve.id, $cve.metrics.cvssMetricV31[0].cvssData.baseScore, $cve.metrics.cvssMetricV31[0].cvssData.vectorString)
+                    $MaxData[$Line] = $arr
+                    $CpeToCveLookup[$cve.id] = $Line
+                }
+                
+            }
+            elseif ($cve.metrics.cvssMetricV30)
+            {
+
+                if ($cve.metrics.cvssMetricV30[0].cvssData.baseScore -gt $max) {
+                    $max = $cve.metrics.cvssMetricV30[0].cvssData.baseScore
+                    $arr = @($cve.id, $cve.metrics.cvssMetricV30[0].cvssData.baseScore, $cve.metrics.cvssMetricV30[0].cvssData.vectorString)
+                    $MaxData[$Line] = $arr
+                    $CpeToCveLookup[$cve.id] = $Line
+                }
+            }
+            else {
+                if ($cve.metrics.cvssMetricV2[0].cvssData.baseScore -gt $max) {
+                    $max = $cve.metrics.cvssMetricV2[0].cvssData.baseScore
+                    $arr = @($cve.id, $cve.metrics.cvssMetricV2[0].cvssData.baseScore, $cve.metrics.cvssMetricV2[0].cvssData.vectorString)
+                    $MaxData[$Line] = $arr
+                    $CpeToCveLookup[$cve.id] = $Line
+                }
+            }
+        } # end of inner for loop
+    } # end of if statements to check for found CVEs
 
     # increment i for next loop and sleep program as requested by the NVD API
-    if ($i -gt 0) 
-    {
+    if ($i -gt 0) {
         Start-Sleep -Seconds 6
     }
     $i++
 }
+    
+# build the URL for the EPSS scores
+$EpssUrl = "https://api.first.org/data/v1/epss?cve="
+$tracker = 1
+foreach($key in $MaxData.Keys) {
+    switch ($MaxData.Count) {
+        {$_ -eq 1} {
+            $EpssUrl += $MaxData[$key][0]
+            break
+        }
+        {$_ -gt $tracker} {
+            $EpssUrl += $MaxData[$key][0] + ","
+            break
+        }
+        {$_ -eq $tracker} {
+            $EpssUrl += $MaxData[$key][0]
+            break
+        }
+    }
+    $tracker++
+}
 
-$data | Format-Table
+# fetch the EPSS score based on the CVE-ID
+try {
+    $EpssScores = Invoke-RestMethod -Uri $EpssUrl
+} catch {
+    Write-Host "Status Code: " $_.Exception.Response.StatusCode.value__
+}
+
+# loop through the returned API data and store them in the data hashmap
+foreach ($item in $EpssScores.data) {
+    $MaxData[$CpeToCveLookup[$item.cve]] += $item.epss
+}
+
+# output the final results
+foreach ($key in $MaxData.Keys) {
+    Write-Host $key ": " $MaxData[$key]
+}
+
